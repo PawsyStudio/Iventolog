af8367df22e1ace29d866b180255eeee32976524
randmoney <RandRicar@yandex.ru>
2025-06-13 15:41:12 +0500

    Реализован блок info

--- Frontend/src/components/home/infoBlock/infoBlock.tsx
+++ Frontend/src/components/home/infoBlock/infoBlock.tsx
@@ -13,6 +13,35 @@ export default function InfoBlock() {
       </div>
 
       <span className={styles.decorativeLabel}>INFORMATION</span>
+
+      <div className={styles.decorBlocksWrapper}>
+        {/* Блок 1 */}
+        <div className={`${styles.decorBlockItem} ${styles.gradientBlock}`}>
+          <div className={styles.textTop}>СОЗДАВАЙ ПРАЗДНИКИ</div>
+          <div className={styles.divider}></div>
+          <div className={styles.textBottom}>
+            Создавай свои уникальные мероприятия
+          </div>
+        </div>
+
+        {/* Блок 2 */}
+        <div className={`${styles.decorBlockItem} ${styles.whiteBlockFirst}`}>
+          <div className={styles.textTopBlue1}>УПРАВЛЯЙ ПРОЦЕССОМ</div>
+          <div className={styles.dividerBlue}></div>
+          <div className={styles.textBottomBlue}>
+            Управляй своими мероприятиями вместе с друзьями и коллегами
+          </div>
+        </div>
+
+        {/* Блок 3 */}
+        <div className={`${styles.decorBlockItem} ${styles.whiteBlockSecond}`}>
+          <div className={styles.textTopBlue2}>КРЕАТИВЬ НАСТОЯЩИЕ ПРАЗДНИКИ</div>
+          <div className={styles.dividerBlue}></div>
+          <div className={styles.textBottomBlue}>
+            Креативь и придумывай свои идеи и мысли благодаря нам
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
